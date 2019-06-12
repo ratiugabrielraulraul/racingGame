4cd5ddfbc08ed133f1091d39fd87c7b4d185353d
Raul <ratiugabrielraul@gmail.com>
2019-06-12 20:39:43 +0300

    added accelerate method for vehicles

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,15 +10,17 @@ public class App
         car.color = "blue";
         car.doorCount = 4;
         car.mileage = 9.5;
+        car.fuelLevel = 50;
         car.running = true;
-        car.travelDistance = 150;
-        car.fuelLevel= 1000;
+
+
 
         Car car2 = new Car();
         car2.name = "Lada";
                 car2.color = "black";
                         car2.doorCount = 2;
         car2.mileage = 5.1;
+        car2.fuelLevel = 60;
         car2.running = false;
 
 
@@ -27,6 +29,7 @@ public class App
         car.engine = engine;
         System.out.println("Our engine");
         System.out.println(car.engine.manufacturing);
+
 
 
 
@@ -41,6 +44,10 @@ public class App
         System.out.println(car3.name);
         System.out.println(car.maxSpeed);
         System.out.println(car.engine);
+
+        double distance = car.accelerate( 60,  0.5);
+        distance = car.accelerate( 60,  0.5);
+        System.out.println("Distance in App.main: " + distance);
 
 
 
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -13,9 +13,29 @@ public class Vehicle {
     double maxSpeed;
     boolean running;
     double fuelLevel;
-    double travelDistance;
+    double traveledDistance;
     LocalDate createdDate;
 
+
+    public double accelerate(double speed, double durationInHours) {
+        System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + "h.");
+        double distance = speed * durationInHours;
+        System.out.println("Traveled " + distance + " km.");
+//        traveledDistance = traveledDistance + distance;
+        //same result as above statement
+        traveledDistance += distance;
+        //logs
+        System.out.println("Total traveled distance " + traveledDistance + " km");
+        double spentFuel = distance / 100 * mileage;
+
+        System.out.println("spent Fuel: " + spentFuel + "1.");
+//        fuelLevel = fuelLevel - spentFuel;
+        //same as above
+        fuelLevel -= spentFuel;
+        System.out.println("Remaining fuel" + fuelLevel + "1.");
+        return distance;
+
+    }
 
 
 
